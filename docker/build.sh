2aca309c37bc219f9d3607666a66b45ea2655ca2
Erik Kristensen <erik@erikkristensen.com>
2023-09-28 08:08:41 -0600

    remove label

--- docker/build.sh
+++ docker/build.sh
@@ -8,7 +8,6 @@ echo "Build docker image restic/restic:latest"
 docker build \
   --rm \
   --pull \
-  --label org.opencontainers.image.source=https://github.com/ekristen/restic \
   --file docker/Dockerfile \
   --tag restic/restic:latest \
   .
